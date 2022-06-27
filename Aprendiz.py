01b6d78d22eb0b677050c74a8e8d4968ff9e7f11
KarenBarbosa2004 <101910201+KarenBarbosa2004@users.noreply.github.com>
2022-06-26 20:23:09 -0500

    Add files via upload

---
+++ Aprendiz.py
@@ -0,0 +1,21 @@
+clase  Aprendiz:
+    def  __init__ ( self , identificacion , nombre , edad , correo , telefono ):
+        uno mismo identificación = identificación
+        uno mismo nombre = nombre
+        uno mismo edad = edad
+        uno mismo correo = correo
+        uno mismo teléfono = teléfono
+        
+    def  calcularEdad ( self ):
+        si  uno mismo . edad >= 18 :
+            print ( ' \n El aprendiz es mayor de edad' )
+        elif  auto . edad < 18 :
+            print ( ' \n El aprendiz es menor de edad ' )
+            
+Aprendiz1 = Aprendiz ( 123 , 'Alberto' , 18 , 'alberto@misena.edu.co' , 3112712896 )
+Aprendiz2 = Aprendiz ( 456 , 'anni' , 17 , 'alberto@misena.edu.co' , 3123456789 )
+imprimir ()
+print ( f'Identificacion del Aprendiz1: { Aprendiz1 . identificacion } \n Nombre: { Aprendiz1 . nombre } \n Edad: { Aprendiz1 . edad } \n Correo: { Aprendiz1 . correo } \n Telefono: { Aprendiz1 . telefono } ' )
+Aprendiz1 . calcularEdad ()
+imprimir ()
+print ( f'Identificacion del Aprendiz2: { Aprendiz2 . identificacion } \n Nombre: { Aprendiz2 . nombre } \n Edad: { Aprendiz2 . edad } \n Correo: { Aprendiz2 . correo } \n Telefono: { Aprendiz2 . telefono } ' )
